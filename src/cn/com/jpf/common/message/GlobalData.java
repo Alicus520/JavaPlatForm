95157d797ca302818f9d04c310ffc14003ca99ca
Alicus <alicus890824@gmail.com>
2012-01-12 19:23:41 +0800

    message

---
+++ src/cn/com/jpf/common/message/GlobalData.java
@@ -0,0 +1,13 @@
+package cn.com.jpf.common.message;
+
+import java.io.Serializable;
+
+public final class GlobalData implements Serializable {
+
+	private static final long serialVersionUID = 7799524244876269906L;
+	
+	public static final String PROJECT_NAME = "JavaPlatForm";
+	
+	public static final String REQUEST_ENCODE = "UTF-8";
+	
+}
---
+++ src/cn/com/jpf/common/message/MsgManager.java
@@ -0,0 +1,144 @@
+package cn.com.jpf.common.message;
+
+import java.util.Hashtable;
+import java.util.Locale;
+
+import org.apache.struts.util.MessageResources;
+import org.apache.struts.util.MessageResourcesFactory;
+
+import cn.com.jpf.common.utils.StringUtil;
+
+/**
+ * Messge Manager class
+ * @author Alicus
+ * @date 2012-1-12
+ * @version 0.1
+ */
+public final class MsgManager {
+	
+	/**
+	 * Default Message Property's name 
+	 * The file is in src folder 
+	 */
+	private static final String MESSAGE_DEFAULT = "MessageResources";
+	
+	/**
+	 * Message Handler Object
+	 */
+	private static Hashtable<String, MsgManager> messagesBundles = new Hashtable<String, MsgManager>();
+	
+	/**
+	 * Defualt Locale type-- English
+	 */
+	private final Locale defaultLocale = Locale.ENGLISH;
+	
+	/**
+	 * MessageResources Object
+	 */
+	private MessageResources resources = null;
+	
+	/**
+	 * Message Bundle Name
+	 * Default name is MessageResources
+	 */
+	private String bundleName = null;
+
+	public MsgManager(String newBundleName, MessageResources newResouce){
+		this.resources = newResouce;
+		setBundleName(newBundleName);
+	}
+	
+	/**
+	 * Get Instance by Singleton Model 
+	 * @return MsgManager
+	 */
+	public final static MsgManager getInstance(){
+		return MsgManager.getInstance(MsgManager.MESSAGE_DEFAULT);
+	}
+	
+	/**
+	 * overload from getInstance method
+	 * @param bundleName
+	 * @return
+	 */
+	public final static MsgManager getInstance(String bundleName){
+		if(bundleName == null){
+			bundleName = MESSAGE_DEFAULT;
+		}
+		
+		MsgManager object = 
+			(MsgManager)MsgManager.messagesBundles.get(bundleName);
+		
+		if(object == null){
+			MsgManager.createBundle(bundleName);
+			
+			object = (MsgManager)MsgManager.messagesBundles.get(bundleName);
+		}
+		
+		return object;
+	}
+	
+	/**
+	 * create Resource By PropertyName
+	 * @param propertyFileName
+	 */
+	public final static void createBundle(String propertyFileName){
+		if(propertyFileName == null){
+			//TODO throw a exception
+		}
+		
+		MessageResources res = null;
+		res = MessageResourcesFactory.createFactory().createResources(propertyFileName);
+		res.setReturnNull(true);
+		
+		MsgManager object = new MsgManager(propertyFileName,res);
+		MsgManager.addBundle(object);
+	}
+	
+	/**
+	 * add a bundle
+	 * @param object
+	 */
+	private static void addBundle(MsgManager object){
+		String bundleName = object.getBundleName();
+		
+		if(MsgManager.messagesBundles.get(bundleName) != null){
+		}else{
+			MsgManager.messagesBundles.put(bundleName, object);
+		}
+		
+	}
+	
+	public String getLocaleString(String key){
+		return this.getLocaleString(key, defaultLocale);
+		
+	}
+	
+	public String getLocaleString(String key, Locale locale){
+		String result = this.resources.getMessage(locale, key);
+		if(StringUtil.isNotEmpty(result)){
+			return result;
+		}else{
+			return key;
+		}
+	}
+	public String getLocaleString(String key, Object[] args){
+		String result = this.resources.getMessage(key, args);
+		return result;
+		
+	}
+	
+	public String getLocaleString(String key, Locale locale, Object[] args){
+		String result = this.resources.getMessage(locale, key, args);
+		return result;
+	}
+	
+	public String getBundleName() {
+		return bundleName;
+	}
+
+	public void setBundleName(String bundleName) {
+		this.bundleName = bundleName;
+	}
+	
+}
