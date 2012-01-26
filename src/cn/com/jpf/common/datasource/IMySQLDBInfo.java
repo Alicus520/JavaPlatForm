acac0e59c58250cd29b23a971f4a51724e51d05f
Alicus <alicus890824@gmail.com>
2012-01-26 20:19:53 +0800

    change the mysql connection information

--- src/cn/com/jpf/common/datasource/IMySQLDBInfo.java
+++ src/cn/com/jpf/common/datasource/IMySQLDBInfo.java
@@ -4,7 +4,7 @@ public interface IMySQLDBInfo {
 	
 	public final static String JDBCDRIVER = "com.mysql.jdbc.Driver";
 	
-	public final static String URL = "jdbc:mysql://localhost:3306/JavaPlatForm";
+	public final static String URL = "jdbc:mysql://localhost:3306/javaplatform";
 	
 	public final static String USERNAME = "root";
 	
